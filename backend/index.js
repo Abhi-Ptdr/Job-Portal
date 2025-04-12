f12c4524dca53d1c494491bf77353716bb95509f
Abhishek Patidar <abhipatidar253@gmail.com>
2025-04-12 23:43:37 +0530

    Backend Initial setup and Created Schema

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+// const express = require('express'); //old way
+import express from "express";     //"type": "module" put this in package.json to use import
+import cors from "cors"; 
+import cookieParser from "cookie-parser";
+import connectDB from "./utils/db.js"; 
+import dotenv from "dotenv";
+dotenv.config({});
+
+const app = express(); //create an instance of express
+
+//middleware
+app.use(express.json()); //parse json data from request body
+app.use(express.urlencoded({ extended: true })); //parse urlencoded data from request body
+app.use(cookieParser()); //parse cookies from request headers
+
+const corsOptions = {
+  origin: "http://localhost:5173", //allow requests from this origin
+  credentials: true, //allow credentials (cookies) to be sent with requests
+};
+
+app.use(cors(corsOptions)); //use cors middleware with options
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => { 
+    connectDB(); //connect to database
+    console.log(`Server is running on port ${PORT}`);
+})
---
+++ backend/models/job.model.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+const jobSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    requirements: [{    //array of skills
+        type: String
+    }],
+    salary: {
+        type: Number,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    jobType: {
+        type: String,
+        required: true
+    },
+    position: {
+        type: Number,
+        required: true
+    },
+    company: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Company',
+        required: true
+    },
+    created_by: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    applications: [{    //array of user ids who applied for the job
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Application'
+    }],
+
+}, { timestamps: true }); //timestamps will add createdAt and updatedAt fields to the schema
+
+export const Job = mongoose.model("Job", jobSchema); //create a model from the schema
