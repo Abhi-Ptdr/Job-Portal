aa67d1893e6321b19282b2c08f791dfbafb685be
Abhishek Patidar <abhipatidar253@gmail.com>
2025-04-21 15:44:53 +0530

    Add AdminJobs component and related functionality; implement job fetching and filtering for admin

---
+++ frontend/src/components/admin/AdminJobs.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react'
+import Navbar from '../shared/Navbar'
+import { Input } from '../ui/input'
+import { Button } from '../ui/button'
+import { useNavigate } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import AdminJobsTable from './AdminJobsTable'
+import useGetAllAdminJobs from '@/hooks/useGetAllAdminJobs'
+import { setSearchJobByText } from '@/redux/jobSlice'
+
+function AdminJobs() {
+  useGetAllAdminJobs();
+  const navigate = useNavigate();
+  
+  const [input, setInput] = useState("");
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setSearchJobByText(input));
+  }, [input]); //we want to call the function for every change in input state. means real time update in state on  every keypress in filter input field in the frontend
+
+  return (
+    <div>
+      <Navbar/>
+        <div className='max-w-6xl mx-auto my-10'>
+            <div className="flex item-center justify-between my-5">
+                <Input className='w-fit' placeholder='Filter by name' onChange={(e) => setInput(e.target.value)}/>
+                <Button onClick={() => navigate('/admin/companies/create')} className='bg-black text-white cursor-pointer'>New Company</Button>
+            </div>
+            <AdminJobsTable/>
+        </div>
+    </div>
+  )
+}
+
+export default AdminJobs
---
+++ frontend/src/components/admin/AdminJobsTable.jsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from 'react'
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '../ui/table'
+import { Edit2, MoreHorizontal } from 'lucide-react'
+import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
+function AdminJobsTable() {
+    const {allAdminJobs, searchJobByText} = useSelector(store => store.job); //to destructure allAdminJobs from jobSlice, we have name:"job" in jobSlice line 4
+    const [filterJobs, setFilterJobs] = useState(allAdminJobs);
+    const navigate = useNavigate();
+    
+    useEffect(() => {
+        const filteredjobs = allAdminJobs.length >= 0 && allAdminJobs.filter((job) => {
+            if(!searchJobByText){
+                return true
+            };
+            return job?.title?.toLowerCase().includes(searchJobByText.toLowerCase()) || job?.company?.name.toLowerCase().includes(searchJobByText.toLowerCase())
+        });
+        setFilterJobs(filteredjobs);
+    }, [allAdminJobs, searchJobByText])
+
+    return (
+        <div>
+            <Table>
+                <TableCaption>A list of your recent posted jobs</TableCaption>
+                <TableHeader className='bg-gray-100'>
+                    <TableRow>
+                        <TableHead>Company Name</TableHead>
+                        <TableHead>Role</TableHead>
+                        <TableHead>Date</TableHead>
+                        <TableHead className='text-right'>Action</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {
+                        filterJobs?.map((job) => (
+                            <TableRow key={job._id}>
+                                <TableCell> {job?.company?.name} </TableCell>
+                                <TableCell> {job?.title} </TableCell>
+                                <TableCell> {job.createdAt.split('T')[0]} </TableCell>
+                                <TableCell className='text-right'>
+                                    <Popover>
+                                        <PopoverTrigger>
+                                            <MoreHorizontal className='cursor-pointer' />
+                                        </PopoverTrigger>
+                                        <PopoverContent className="w-25 bg-white">
+                                            <div onClick={() => navigate(`${job._id}`)} className='flex item-center gap-2 w-fit cursor-pointer'>
+                                                <Edit2 className='w-4' />
+                                                <span>Edit</span>
+                                            </div>
+                                        </PopoverContent>
+                                    </Popover>
+                                </TableCell>
+                            </TableRow>
+                        ))
+                    }
+                </TableBody>
+            </Table>
+        </div>
+    )
+}
+
+export default AdminJobsTable
