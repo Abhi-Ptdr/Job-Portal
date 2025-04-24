eb37c95f342dc549a0c3c357c1e4369678dee7b1
Abhishek Patidar <abhipatidar253@gmail.com>
2025-04-24 23:26:21 +0530

    Implemented job filtering functionality in Jobs component

--- frontend/src/components/FilterCard.jsx
+++ frontend/src/components/FilterCard.jsx
@@ -1,46 +1,59 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { RadioGroup, RadioGroupItem } from './ui/radio-group'
-import { data } from 'react-router-dom'
+import { setSearchedQuery } from '@/redux/jobSlice';
+import { useDispatch } from 'react-redux';
 
 const filterData = [
     {
         filterType: 'Location',
-        array: ["Delhi", "Mumbai", "Bangalore", "Pune", "Chennai", "Hyderabad"]
+        array: ["Delhi", "Mumbai", "Bangalore", "Pune", "Hyderabad"]
     },
     {
-        filterType: 'Experience',
-        array: ["0-1", "1-2", "2-3", "3-4", "4-5"]
-    },
-    {
-        filterType: 'Salary',
-        array: ["0-1 LPA", "1-2 LPA", "2-3 LPA", "3-4 LPA", "4-5 LPA"]
+        filterType: 'Job Role',
+        array: ["Frontend Developer", "Backend Developer", "Data Scientist", "Machine Learning Engineer"]
     },
     {
         filterType: 'Job Type',
         array: ["Full Time", "Part Time", "Internship"]
     },
-    {
-        filterType: 'Company',
-        array: ["TCS", "Wipro", "Accenture", "Cognizant"]
-    },
+    // {
+    //     filterType: 'Experience',
+    //     array: ["0-1", "1-2", "2-3", "3-4", "4-5"]
+    // },
+    // {
+    //     filterType: 'Salary',
+    //     array: ["0-6 LPA", "6-10 LPA", "10-15 LPA", "15+ LPA"]
+    // },
 ]
 
 function FilterCard() {
+    const [selectedValue, setSelectedValue] = useState('');
+    const dispatch = useDispatch();
+
+    const changeHandler = (value) => {
+        setSelectedValue(value);
+    }
+
+    useEffect(()=>{
+        dispatch(setSearchedQuery(selectedValue));
+    },[selectedValue]);
+
   return (
     <div className='w-full bg-white p-3 rounded-md'>
       <h1 className='font-bold text-lg'>Filter Jobs</h1>
       <hr className='mt-3'/>
-      <RadioGroup>
+      <RadioGroup value={selectedValue} onValueChange={changeHandler}>           {/*J.S. has onValueChange Function for radio buttons*/}
         {
             filterData.map((data, index) => (
-                <div>
+                <div key={index}>
                     <h1 className='font-bold text-lg'>{data.filterType}</h1>
                     {
-                        data.array.map((item, index) => {
+                        data.array.map((item, idx) => {
+                            const itemId = `id${index}-${idx}` //we can generate unique key for each item anyhow.
                             return (
-                                <div className='flex items-center space-x-2 my-2'>
-                                    <RadioGroupItem value={item} className='w-3 h-3'/>
-                                    <label className='cursor-pointer'>{item}</label>
+                                <div key={itemId} className='flex items-center space-x-2 my-2'>
+                                    <RadioGroupItem value={item} id={itemId} className='w-3 h-3'/>
+                                    <label htmlFor={itemId} className='cursor-pointer'>{item}</label>
                                 </div>
                             )
                         })
